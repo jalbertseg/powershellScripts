3989f1a16b29443741dde964afec1f224cf67f79
javier <javier.albert@gmail.com>
2017-07-21 20:50:57 +0200

    Minor changes

--- Convert.ps1
+++ Convert.ps1
@@ -1,24 +1,11 @@
 param (
-    [string]$path = $(throw "Path parameter is required."),    
-    [string]$from = "m4a",
+    [string]$path = $(throw "Path parameter is required."),
+    [string]$from = "m4a", #$from = '.ogg' #mp4a, mp4, ogg... Music File type
     [switch]$delete = $false
  )
-Write-Host "Music File converter"
-
-#Set the path to crawl
-# Cambiar el directorio a convertir
-#$path = '' #put your directory here like C:\XXXXX
-
-Write-Host "Directory: $path"
-
-#The source or input file format
-#Tipo de archivo a convertir del directorio seleccionado
-#$from = '.ogg' #mp4a, mp4, ogg... Music File type
-
-Write-Host "Filetype: $from"
-
-#Recorre el directorio seleccionado recursivamente seleccionando los ficheros $from
-
+Write-Host " ***** Music File converter *****"
+Write-Host "   - Directory: $path"
+Write-Host "    - Filetype: $from"
 Write-Host "***** Start file conversion *****"
 $ffmpeg = "C:\ffmpeg\bin\ffmpeg.exe"
 if(Test-Path $ffmpeg){
@@ -47,5 +34,4 @@ if(Test-Path $ffmpeg){
 else{
   Write-Host "You MUST install ffmpeg into C:\ffmpeg\"
 }
-
-    Write-Host " ***** End file conversion ******"
+Write-Host " ***** End file conversion ******"
